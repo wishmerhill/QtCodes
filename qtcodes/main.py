f1deaf68e6ee798891824eb837c957b61bac0fd2
Wishmerhill <wishmerhill@nousefornames.it>
2016-12-08 12:01:28 +0100

    - quit confirmation dialog added - TODO: vCard check - vCard tab added. TODO: let it work!

--- qtcodes/main.py
+++ qtcodes/main.py
@@ -18,12 +18,29 @@ try:
     import pyqrcode
 except ImportError:
     if DEBUG_MODE:
-        print('*** Warning *** pyqrcodes not found - Please install before going on! ***')
+        print('DEBUG: *** Warning *** pyqrcodes not found - Please install before going on! ***')
     exit()
 
 import PyQt4.uic
 from PyQt4 import QtGui
 import signal
+import vobject
+
+
+class quitDialog(QtGui.QDialog):
+    """
+    the quit dialog
+    """
+
+    def __init__(self, parent=None):
+        super(quitDialog, self).__init__(parent)
+
+        # load the ui
+        self.ui = PyQt4.uic.loadUi('ui/quitDialog.ui', self)
+
+        self.pushButtonYes.clicked.connect(app.quit)
+        self.pushButtonNo.clicked.connect(self.close)
+
 
 class MainWindow(QtGui.QMainWindow):
     """
@@ -35,11 +52,19 @@ class MainWindow(QtGui.QMainWindow):
         # load the ui
         self.ui = PyQt4.uic.loadUi('ui/mainWindow.ui', self)
 
+        # Setting the Text tab as active. Better so.
+        if DEBUG_MODE:
+            print("DEBUG: Setting plain text tab as active")
+        self.tabWidget.setCurrentIndex(0)
+
         self.generateButton.clicked.connect(self.generateQR)
 
         self.selectFileButton.clicked.connect(self.selectFile)
 
+        self.actionQuit.triggered.connect(self.doClose)
+
         self.filename = 'filename.png'
+
 
     def generateQR(self):
         """
@@ -48,18 +73,45 @@ class MainWindow(QtGui.QMainWindow):
         data = unicode(self.textEdit.toPlainText())
         if DEBUG_MODE:
             print(data)
+
+        # create the QR
         qr = pyqrcode.create(data, error='H', mode=None, encoding=None)
 
-        qr.png(self.filename, scale=2)
+        # generate the PNG file *** TODO: let the use choose filetype, scale and other parameters.
+        qr.png(self.filename, scale=3)
+
+        # store the file as pixmap
         pixmap = QtGui.QPixmap(self.filename)
 
         self.qrPreview.setPixmap(pixmap)
 
+
         self.qrPreview.adjustSize()
 
     def selectFile(self):
+        '''
+        Just a small function to open a file dialog
+        '''
+
         self.filenameEdit.setText(QtGui.QFileDialog.getOpenFileName())
+
+        # encode the resulting filename as UNICODE text
         self.filename=unicode(self.filenameEdit.text())
+
+    def doClose(self):
+        '''
+        Let's ask to confirm quit if there is still some text in the text area
+        '''
+
+        data = unicode(self.textEdit.toPlainText())
+        if not data:
+            self.close()
+            if DEBUG_MODE:
+                print('Bye!')
+        else:
+            confirmQuit = quitDialog()
+            confirmQuit.show()
+
 
 
 
